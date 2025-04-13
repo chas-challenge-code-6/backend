6b5274ab4d171d515ae7774212687a08755a3bc4
elinstella <elin_elsa@hotmail.com>
2025-04-13 20:22:34 +0200

    Tillägg av npm run dev och bugg fix

--- backend-sentinel/middlewares/validateSensorData.js
+++ backend-sentinel/middlewares/validateSensorData.js
@@ -1,8 +1,9 @@
-module.exports = (req, res, next) => {
-    const { device_id, timestamp, sensors } = req.body;
-    if (!device_id || !timestamp || !sensors) {
-      return res.status(400).json({ error: "Missing required fields" });
-    }
-    next();
-  };
-  
+const validateSensorData = (req, res, next) => {
+  const { device_id, timestamp, sensors } = req.body;
+  if (!device_id || !timestamp || !sensors) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+  next();
+};
+
+export default validateSensorData;
