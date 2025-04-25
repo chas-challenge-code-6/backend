29be329415f9c641e9111fb931b601d02d69558c
milton-web <milton.tuti@chasacademy.se>
2025-04-25 10:12:20 +0200

    added register user

--- backend-sentinel/app.js
+++ backend-sentinel/app.js
@@ -7,6 +7,12 @@ import usersRouter from './routes/users.js';
 import dataRoutes from './routes/data.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import authRoutes from './routes/auth.js';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 8766;
@@ -21,12 +27,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 // Routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', dataRoutes);
-
+app.use('/auth', authRoutes);
+app.use('/data', dataRoutes);
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
