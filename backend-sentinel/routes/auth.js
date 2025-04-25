4c4979012a0b56bccf53f1bbb9d3f722057dcdaf
milton-web <milton.tuti@chasacademy.se>
2025-04-25 11:26:23 +0200

    added GET me and connected token to username

--- backend-sentinel/routes/auth.js
+++ backend-sentinel/routes/auth.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/authController.js';
+import { loginUser, registerUser, getMe } from '../controllers/authController.js';
+import { authenticateToken } from '../middlewares/authenticateToken.js'; // adjust path as needed
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/me', authenticateToken, (req, res) => {
+    res.json({ user: req.user });
+  });
 
 export default router;
