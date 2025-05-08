b642fbe04684e13fe9f0955da28fe3b66dc78825
Simon Jacobsson <simon.jacobsson@chasacademy.se>
2025-05-08 13:18:37 +0200

    Small changes
    Cleaned up the code a bit, small cahnges

--- backend-sentinel/config/database.js
+++ backend-sentinel/config/database.js
@@ -7,13 +7,12 @@ const sequelize = new Sequelize({
   dialect: 'postgres',
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'sensor_data_db', // The name of your database
-  username: process.env.DB_USER || 'your_pg_user', // PostgreSQL username
-  password: process.env.DB_PASSWORD || 'your_pg_password', // PostgreSQL password
-  logging: false, // Set to true to log SQL queries
+  database: process.env.DB_NAME || 'Sentinel',
+  username: process.env.DB_USER || 'your_pg_user',
+  password: process.env.DB_PASSWORD || 'your_pg_password',
+  logging: false,
 });
 
-// Test the connection (optional, but helpful for debugging)
 sequelize
   .authenticate()
   .then(() => {
