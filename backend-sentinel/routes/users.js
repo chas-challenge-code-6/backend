36c9e4dc6633e1b4883437092e1b19446770bd1a
elinstella <elin_elsa@hotmail.com>
2025-05-20 12:37:09 +0200

    Updated user

--- backend-sentinel/routes/users.js
+++ backend-sentinel/routes/users.js
@@ -1,19 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import mongoose from 'mongoose';
-
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email:    { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-
-export const User = mongoose.model('User', userSchema);
 
 export default router;
